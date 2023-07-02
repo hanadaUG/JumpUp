99875eb8737b2c03b9b09a58b7a7e8af6151344f
hanadaUG <hanadaUG@gmail.com>
2023-07-03 03:56:34 +0900

    グリッド表示

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    // グリッド
+    [SerializeField] private int height = 20, width = 9, header = 2, footer = 4;
+
+    // グリッドを表現する2次元配列の作成
+    private Transform[,] _grid;
+
+    // 1マス分のスプライト
+    [SerializeField] private Transform emptySprite;
+
+
+    private void Awake()
+    {
+        _grid = new Transform[width, height];
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateGrid()
     {
-        
+        if (emptySprite)
+        {
+            for (int y = 0; y < height - header - footer; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Transform clone = Instantiate(emptySprite, new Vector3(x, y, 0), Quaternion.identity);
+                    clone.transform.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("emptySprite not set!");
+        }
     }
 }
